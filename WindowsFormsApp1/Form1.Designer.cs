913f32d3925dc4187d60d0f6f23659b41678c6ba
lukacorujo06 <lukacorujo2100@gmail.com>
2021-10-02 22:21:51 -0300

    Aplicacion 4
    Message Box

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_usuario = new System.Windows.Forms.Label();
+            this.lbl_contraseña = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.text_contraseña = new System.Windows.Forms.TextBox();
+            this.btn_iniciar_sesion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_usuario
+            // 
+            this.lbl_usuario.AutoSize = true;
+            this.lbl_usuario.Location = new System.Drawing.Point(181, 66);
+            this.lbl_usuario.Name = "lbl_usuario";
+            this.lbl_usuario.Size = new System.Drawing.Size(43, 13);
+            this.lbl_usuario.TabIndex = 0;
+            this.lbl_usuario.Text = "Usuario";
+            // 
+            // lbl_contraseña
+            // 
+            this.lbl_contraseña.AutoSize = true;
+            this.lbl_contraseña.Location = new System.Drawing.Point(178, 98);
+            this.lbl_contraseña.Name = "lbl_contraseña";
+            this.lbl_contraseña.Size = new System.Drawing.Size(61, 13);
+            this.lbl_contraseña.TabIndex = 1;
+            this.lbl_contraseña.Text = "Contraseña";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(245, 66);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(100, 20);
+            this.txt_usuario.TabIndex = 2;
+            // 
+            // text_contraseña
+            // 
+            this.text_contraseña.Location = new System.Drawing.Point(245, 98);
+            this.text_contraseña.Name = "text_contraseña";
+            this.text_contraseña.PasswordChar = '*';
+            this.text_contraseña.Size = new System.Drawing.Size(100, 20);
+            this.text_contraseña.TabIndex = 3;
+            // 
+            // btn_iniciar_sesion
+            // 
+            this.btn_iniciar_sesion.Location = new System.Drawing.Point(196, 135);
+            this.btn_iniciar_sesion.Name = "btn_iniciar_sesion";
+            this.btn_iniciar_sesion.Size = new System.Drawing.Size(126, 23);
+            this.btn_iniciar_sesion.TabIndex = 4;
+            this.btn_iniciar_sesion.Text = "Iniciar sesion";
+            this.btn_iniciar_sesion.UseVisualStyleBackColor = true;
+            this.btn_iniciar_sesion.Click += new System.EventHandler(this.btn_iniciar_sesion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 275);
+            this.Controls.Add(this.btn_iniciar_sesion);
+            this.Controls.Add(this.text_contraseña);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.lbl_contraseña);
+            this.Controls.Add(this.lbl_usuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_usuario;
+        private System.Windows.Forms.Label lbl_contraseña;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox text_contraseña;
+        private System.Windows.Forms.Button btn_iniciar_sesion;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        usuario objUsuario;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objUsuario = new usuario();
+
+            objUsuario.nombre = "luka06";
+            objUsuario.contraseña = "messi10";
+        }
+
+        private void btn_iniciar_sesion_Click(object sender, EventArgs e)
+        {
+            if ((objUsuario.nombre == "luka06") && (objUsuario.contraseña == "messi10"))
+            {
+                MessageBox.Show("Bienvenido " + objUsuario.nombre);
+            } 
+            else if ((objUsuario.nombre == "Luka06") && (objUsuario.contraseña != "messi10"))
+            {
+                MessageBox.Show("Contraseña incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if ((objUsuario.nombre != "Luka06") && (objUsuario.contraseña == "messi10"))
+            {
+                MessageBox.Show("Nombre de usuario incorrecto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if ((objUsuario.nombre != "Luka06") && (objUsuario.contraseña != "messi10"))
+            {
+                MessageBox.Show("Nombre de usuario y contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    class usuario
+    {
+        public string nombre, contraseña;
+    }
+}
